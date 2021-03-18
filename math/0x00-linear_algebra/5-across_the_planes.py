223881725ec9d5cec57b91074fa98808e7da60b0
Abdelhamid elbadri <39597183+Elbadri0@users.noreply.github.com>
2021-03-18 14:18:10 +0100

    Update 5-across_the_planes.py

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
+"""
+the sum of two matrices
+"""
+
+
 def add_matrices2D(mat1, mat2):
+    """
+    enter a matrix
+    and Returns the sum of the two matrices
+    """
     if len(mat1) != len(mat2):
         return None
-    elif len(mat1[0]) != len(mat2[0]):
+    if len(mat1[0]) != len(mat2[0]):
         return None
-    else:
-        new_mat = [[mat1[i][j] + mat2[i][j]
-                    for j in range(len(mat1[0]))] for i in range(len(mat1))]
-        for count in new_mat:
-            return new_mat
+    return [[(mat1[i][j] + mat2[i][j]) for j in range(len(mat1[0]))]
+            for i in range(len(mat1))]
