b80c6a6fb2c2573ef2b1b3190d38c6c218796185
Abdelhamid elbadri <39597183+Elbadri0@users.noreply.github.com>
2021-03-18 14:52:15 +0100

    Update 102-squashed_like_sardines.py

--- math/0x00-linear_algebra/102-squashed_like_sardines.py
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -1,40 +1,38 @@
 #!/usr/bin/env python3
-"""
-concatenates two matrices
-"""
+"""Contains the add_matrices function"""
 
 
-def matrix_shape(matrix):
-    """
-    enter a matrix
-    Returns the shape as a list of integers
-    """
-    if type(matrix[0]) is not list:
-        return [len(matrix)]
-    else:
-        return [len(matrix)] + matrix_shape(matrix[0])
+def shape(matrix):
+    """ calculates the matrix shape """
+    shape = [len(matrix)]
+    while type(matrix[0]) == list:
+        shape.append(len(matrix[0]))
+        matrix = matrix[0]
+    return shape
 
 
-def concat_recursive(mat1, mat2, axe):
-    """
-    enter a matrix
-    Returns a concatenated matrix
-    """
-    result = []
-    if axe == 0:
-        result = mat1 + mat2
-        return result
+def rec_cat_matrices(mat1, mat2, axis, depth):
+    """recursivly concat two matrices"""
+    matrix = []
+
+    if depth == axis:
+        matrix = mat1 + mat2
+        return matrix
+
     for i in range(len(mat1)):
-        result.append(concat_recursive(mat1[i], mat2[i], axe - 1))
-    return result
+        matrix.append(rec_cat_matrices(mat1[i], mat2[i], axis, depth + 1))
+    return matrix
 
 
 def cat_matrices(mat1, mat2, axis=0):
-    """
-    enter a matrix
-    Returns the resulting matrix
-    """
-    if len(matrix_shape(mat1)) > axis and len(matrix_shape(mat2)) > axis:
-        return concat_recursive(mat1, mat2, axis)
-    else:
+    """that concatenates two matrices along a specific axis"""
+
+    shape1 = shape(mat1)
+    shape2 = shape(mat2)
+
+    shape1.pop(axis)
+    shape2.pop(axis)
+    if shape1 != shape2 or axis >= len(shape1) + 1:
         return None
+    depth = 0
+    return rec_cat_matrices(mat1, mat2, axis, depth)
