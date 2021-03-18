3f8c1190e0ae1f8d7b7d750480f846f9c1762b09
Abdelhamid elbadri <39597183+Elbadri0@users.noreply.github.com>
2021-03-18 14:03:23 +0100

    Update 2-size_me_please.py

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
+"""
+Module to calculate the shape of a matrix
+"""
+
+
 def matrix_shape(matrix):
+    """
+    Needs a matrix as input
+    Returns the shape as a list of integers
+    """
     if type(matrix[0]) is not list:
         return [len(matrix)]
     else:
