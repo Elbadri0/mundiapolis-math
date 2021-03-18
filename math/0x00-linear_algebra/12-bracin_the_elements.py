7f5cfff6d937fd8af3de70192134d7973e804c52
Abdelhamid elbadri <39597183+Elbadri0@users.noreply.github.com>
2021-03-18 14:34:19 +0100

    Update 12-bracin_the_elements.py

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+"""
+operations on matrices
+"""
+
+
 def np_elementwise(mat1, mat2):
-    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
+    """
+    enter a matrix
+    and Returns the result
+    """
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
