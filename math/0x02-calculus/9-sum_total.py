c57dc830c3dc45ef38feecead960078de0f8967b
Abdelhamid elbadri <39597183+Elbadri0@users.noreply.github.com>
2021-03-27 09:20:08 +0100

    Add files via upload

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+Calculates the sum of i to the power of 2 from i equals 1 to n.
+'''
+
+
+def summation_i_squared(n):
+    '''
+    Calculates the sum of i to the power of 2 from i equals 1 to n.
+
+    Args:
+        n: upper bound summation.
+
+    Returns:
+        the sum of i to the power of 2 from i equals 1 to n
+    '''
+
+    if not isinstance(n, int) or n <= 0:
+        return None
+    return int((n*(n + 1)*(2*n + 1)) / 6)
